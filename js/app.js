bcd7b96206e3976783e22bf134457c357e12e7e9
Rashmi <rashmimalpande@gmail.com>
2017-01-25 17:07:51 +0530

    Create on click event on One Player btn

---
+++ js/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("#one-pl").click(function(){
+        $(".landing-screen").css("display", "none");
+        $(".choose-symbol").css("display","block");
+    });
+});
