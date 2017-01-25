bf9cab557ddcd3948912fa9408f416e170a5924e
Rashmi <rashmimalpande@gmail.com>
2017-01-25 17:46:31 +0530

    Change some styling of the elements

--- js/app.js
+++ js/app.js
@@ -3,4 +3,9 @@ $(document).ready(function(){
         $(".landing-screen").css("display", "none");
         $(".choose-symbol").css("display","block");
     });
+
+    $(".back-btn").click(function(){
+        $(".landing-screen").css("display", "block");
+        $(".choose-symbol").css("display","none");
+    });
 });
