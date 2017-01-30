c8d8b39b326a8e8a42d4cd9d7af721c8308abccc
Rashmi Malpande <rashmimalpande@gmail.com>
2017-01-30 18:14:08 +0530

    create a canvas for the game board

--- js/app.js
+++ js/app.js
@@ -8,4 +8,69 @@ $(document).ready(function(){
         $(".landing-screen").css("display", "block");
         $(".choose-symbol").css("display","none");
     });
+
+    $("#x").click(function(){
+        $(".game-board").css("display","block");
+        $(".landing-screen").css("display", "none");
+        $(".choose-symbol").css("display","none");
+        draw();
+        
+    });
 });
+
+
+function draw(){
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
+context.strokeStyle = '#fff';
+
+context.beginPath();
+context.moveTo(0, 150);
+context.lineTo(100, 150);
+context.stroke();
+
+ context.beginPath();
+context.moveTo(100, 150);
+context.lineTo(100, 50);
+context.stroke();
+
+context.beginPath();
+context.moveTo(100, 150);
+context.lineTo(200, 150);
+context.stroke();
+
+context.beginPath();
+context.moveTo(200, 150);
+context.lineTo(200, 50);
+context.stroke();
+
+context.beginPath();
+context.moveTo(200, 150);
+context.lineTo(300, 150);
+context.stroke();
+
+context.beginPath();
+context.moveTo(100, 150);
+context.lineTo(100, 250);
+context.stroke();
+
+context.beginPath();
+context.moveTo(200, 150);
+context.lineTo(200, 250);
+context.stroke();
+
+context.beginPath();
+context.moveTo(0, 250);
+context.lineTo(300, 250);
+context.stroke();
+
+context.beginPath();
+context.moveTo(100, 250);
+context.lineTo(100, 350);
+context.stroke();
+
+context.beginPath();
+context.moveTo(200, 250);
+context.lineTo(200, 350);
+context.stroke();
+}
